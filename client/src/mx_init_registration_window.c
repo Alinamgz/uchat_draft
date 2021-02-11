c95fcad217b1cb8dac8e146eb3c9210a4878be43
Alina Mazur <amazur@e1r5p8.unit.ua>
2021-02-11 21:40:35 +0200

    fixed segfault when press Enter in form input

--- client/src/mx_init_registration_window.c
+++ client/src/mx_init_registration_window.c
@@ -20,11 +20,13 @@ void mx_init_registration_window(t_client *client) {
 
 static void init_widgets(GtkBuilder *builder, t_client *client) {
     if (!builder) return;
-
+    
+// ----------- window ---------------
     client->ui->registration_window = GTK_WIDGET(gtk_builder_get_object(builder, "registration_window"));
     if (!client->ui->registration_window)
         g_critical("Can't get registration_window");
 
+// ----------- username ---------------
     client->ui->r_username_entry = GTK_WIDGET(gtk_builder_get_object(builder, "r_username_entry"));
     if (!client->ui->r_username_entry)
         g_critical("Can't get r_username_entry");
@@ -33,6 +35,7 @@ static void init_widgets(GtkBuilder *builder, t_client *client) {
     if (!client->ui->r_username_status)
         g_critical("Can't get r_username_status");
 
+// ----------- password ---------------
     client->ui->r_pass_entry = GTK_WIDGET(gtk_builder_get_object(builder, "r_pass_entry"));
     if (!client->ui->r_pass_entry)
         g_critical("Can't get r_pass_entry");
@@ -41,6 +44,7 @@ static void init_widgets(GtkBuilder *builder, t_client *client) {
     if (!client->ui->r_pass_status)
         g_critical("Can't get r_pass_status");
 
+// ----------- confirm password ---------------
     client->ui->conf_pass_entry = GTK_WIDGET(gtk_builder_get_object(builder, "conf_pass_entry"));
     if (!client->ui->conf_pass_entry)
         g_critical("Can't get conf_pass_entry");
@@ -49,6 +53,7 @@ static void init_widgets(GtkBuilder *builder, t_client *client) {
     if (!client->ui->conf_pass_status)
         g_critical("Can't get conf_pass_status");
 
+// ----------- firstname ---------------
     client->ui->firstname_entry = GTK_WIDGET(gtk_builder_get_object(builder, "firstname_entry"));
     if (!client->ui->firstname_entry)
         g_critical("Can't get firstname_entry");
@@ -57,6 +62,7 @@ static void init_widgets(GtkBuilder *builder, t_client *client) {
     if (!client->ui->firstname_status)
         g_critical("Can't get firstname_status");
 
+// ----------- lastname ---------------
     client->ui->lastname_entry = GTK_WIDGET(gtk_builder_get_object(builder, "lastname_entry"));
     if (!client->ui->lastname_entry)
         g_critical("Can't get lastname_entry");
@@ -65,27 +71,45 @@ static void init_widgets(GtkBuilder *builder, t_client *client) {
     if (!client->ui->lastname_status)
         g_critical("Can't get lastname_status");
 
+// ----------- submit btn ---------------
     client->ui->register_btn = GTK_WIDGET(gtk_builder_get_object(builder, "register_btn"));
     if (!client->ui->register_btn)
         g_critical("Can't get register_btn");
 
+// ----------- change form btn ---------------
     client->ui->show_login_btn = GTK_WIDGET(gtk_builder_get_object(builder,"show_login_btn"));
     if (!client->ui->show_login_btn)
         g_critical("Can't get show_login_btn");
 
 }
+
 static void connect_signals(t_client *client) {
-    // TODO: what's the point in signal_conect "activate" for entry?
+// ------------------ entry --------------------
     g_signal_connect(G_OBJECT(client->ui->r_username_entry),
                               "activate",
-                              G_CALLBACK(mx_get_input_values),
+                              G_CALLBACK(mx_submit_registration_handler),
                               client);
 
     g_signal_connect(G_OBJECT(client->ui->r_pass_entry),
                               "activate",
-                              G_CALLBACK(mx_get_input_values),
+                              G_CALLBACK(mx_submit_registration_handler),
+                              client);
+    g_signal_connect(G_OBJECT(client->ui->conf_pass_entry),
+                              "activate",
+                              G_CALLBACK(mx_submit_registration_handler),
                               client);
 
+    g_signal_connect(G_OBJECT(client->ui->firstname_entry),
+                              "activate",
+                              G_CALLBACK(mx_submit_registration_handler),
+                              client);
+
+    g_signal_connect(G_OBJECT(client->ui->lastname_entry),
+                              "activate",
+                              G_CALLBACK(mx_submit_registration_handler),
+                              client);
+
+// ------------------ btns --------------------
     g_signal_connect(G_OBJECT(client->ui->register_btn),
                               "clicked",
                               G_CALLBACK(mx_submit_registration_handler),
