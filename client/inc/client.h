d0ae79d92d3986fe972fd2617ea795e39a10075c
Alina Mazur <amazur@e1r8p14.unit.ua>
2021-02-15 23:07:04 +0200

    addde auth resp persinf=g and proceeding on client

--- client/inc/client.h
+++ client/inc/client.h
@@ -18,6 +18,7 @@
 #define BAD_REQ 400
 #define UNAUTHORIZED 401
 #define NOT_FOUND 404
+#define INTERNAL_SRVR_ERR 500
 
 // ===== includes =====
 #include <errno.h>
@@ -90,6 +91,13 @@ typedef struct s_ui {
 	GtkWidget *show_login_btn;
 }			   t_ui;
 
+typedef struct s_self {
+	char *username;
+	char *first_name;
+	char *last_name;
+	int uid;
+}			   t_self;
+
 typedef struct s_client {
 	pthread_t connection_th;
 	pthread_t auth_th;
@@ -104,6 +112,7 @@ typedef struct s_client {
 	t_ui *ui;
 	t_scene scene;
 	t_scene prev_scene;
+	t_self *self;
 
 	cJSON *auth_req;
 }			   t_client;
@@ -147,5 +156,8 @@ const gchar *mx_get_n_check_entry(char **err, bool is_req, gpointer entry, gpoin
 // create req JSON
 cJSON *mx_create_auth_req(t_client *client, t_raw_inputs *inputs, t_scene type);
 
+// processing auth response
+void mx_parse_n_proceed_auth_response(t_client *client, char *res_buf);
+
 void *mx_do_auth_th(void *arg);
- void exit_gtk(GtkWidget *widget, void *param);
+void exit_gtk(GtkWidget *widget, void *param);
--- client/src/mx_do_auth_th.c
+++ client/src/mx_do_auth_th.c
@@ -1,28 +1,24 @@
 #include "client.h"
 
+static void send_req(t_client *client);
+
 void *mx_do_auth_th(void *arg) {
     t_client *client = (t_client *)arg;
 
     pthread_mutex_lock(&client->connection_mut);
 
-    char *send_buf = cJSON_PrintUnformatted(client->auth_req);
     size_t res_len = BUF_SZ;
     char res_buf[res_len];
-
-    if (send(client->sock_fd, send_buf, strlen(send_buf), 0) < 0) {
-        write(STDERR_FILENO, "Err: sending auth failed\n", strlen("Err: sending auth failed\n"));
-    }
-    else {
-        printf("Sent to server: %s\n", send_buf);
-    }
-
-    free(send_buf);
-
-// TODO: you stopped here!
     memset(res_buf, 0, BUF_SZ);
+
+    send_req(client);
+
     if (recv(client->sock_fd, res_buf, res_len, 0) > 0) {
+    // TODO: Del it:
         printf("Received answer: %s\n", res_buf);
         fflush(stdout);
+    // --------------------------------------
+        mx_parse_n_proceed_auth_response(client, res_buf);
     }
     else {
         write(STDERR_FILENO, "Err: recv auth response failed\n", strlen("/!\\Err: recv auth response failed\n"));
@@ -30,4 +26,14 @@ void *mx_do_auth_th(void *arg) {
     cJSON_Delete(client->auth_req);
     pthread_mutex_unlock(&client->connection_mut);
     return NULL;
+}
+
+static void send_req(t_client *client) {
+    char *send_buf = cJSON_PrintUnformatted(client->auth_req);
+
+    if (send(client->sock_fd, send_buf, strlen(send_buf), 0) < 0) {
+        write(STDERR_FILENO, "Err: sending auth failed\n", strlen("Err: sending auth failed\n"));
+    }
+
+    free(send_buf);
 }
